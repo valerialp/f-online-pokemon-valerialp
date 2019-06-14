7bd53909dc07441142b26fae95a7f3769132eef7
valerialp <valeria.lorentep@gmail.com>
2019-06-14 12:03:46 +0200

    Basis estructure

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
+}
+
+export default App;
