e5ff9740f0718024b5c00be6e7c18ca2298118c2
valerialp <valeria.lorentep@gmail.com>
2019-06-14 22:30:33 +0200

    Delete services, make fetch in App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,31 +1,61 @@
-import React from 'react';
-import './styles.scss';
-import Filters from '../Filters';
-import Results from '../Results';
+import React from "react";
+import "./styles.scss";
+import Filters from "../Filters";
+import Results from "../Results";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data:[{name:'pikachu'}, {name:'mew'}, {name:'vulpix'}],
-      pokemon:'',
-    }
+      data: [],
+      pokemon: ""
+    };
 
     this.handleChangeInputSearch = this.handleChangeInputSearch.bind(this);
   }
 
-  handleChangeInputSearch(e){
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData(){
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=25")
+      .then(response => response.json())
+      .then(data => {
+        const dataIndex = data.results.map((item, index) => {
+          return { ...item, id: index + 1 };
+        });
+  
+        const dataInfo = [];
+  
+        return dataIndex.map(item =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${item.id}`)
+            .then(response => response.json())
+            .then(item => {
+              dataInfo.push(item);
+              return dataInfo;
+            })
+            .then(dataInfo => this.setState({info:dataInfo}))
+        );
+      });
+
+  }
+
+  handleChangeInputSearch(e) {
     const { value } = e.target;
     this.setState({
-      pokemon: value,
-    })
+      pokemon: value
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Filters pokemon={this.state.pokemon} onChangeInput={this.handleChangeInputSearch}/>
-        <Results pokemon={this.state.pokemon} data={this.state.data}/>
+        <Filters
+          pokemon={this.state.pokemon}
+          onChangeInput={this.handleChangeInputSearch}
+        />
+        <Results pokemon={this.state.pokemon} data={this.state.data} />
       </div>
     );
   }
