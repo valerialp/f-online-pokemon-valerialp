7295cbb02bf863a07edf2fffdb48d366ba5533d4
valerialp <valeria.lorentep@gmail.com>
2019-06-20 00:04:58 +0200

    Add style evolution

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -9,14 +9,14 @@ class Card extends React.Component {
       types,
       sprites: { front_default }
     } = this.props.item;
-    const {evo} =this.props;
+    const { evo } = this.props;
     return (
       <Fragment>
         <div className="card__container-top">
           <img className="image-pokemon" src={front_default} alt={name} />
           <p className="id-pokemon">ID / {id}</p>
         </div>
-        <div className="card__container-bottom">
+        <div className="card__container-medium">
           <h3 className="name-pokemon">{name}</h3>
           <div className="types__container">
             {types.map(item => {
@@ -25,13 +25,14 @@ class Card extends React.Component {
               );
             })}
           </div>
-          {evo && evo.evolves_from_species ?
-          <div>
-            <p>Evoluciona de:</p>
-            <p>{evo.evolves_from_species.name}</p>
+          <div className="card__container-bottom">
+            {evo && evo.evolves_from_species ? (
+              <div className="evolution-pokemon">
+                <p>Evoluciona de:</p>
+                <p className="name-pokemon__bottom">{evo.evolves_from_species.name}</p>
+              </div>
+            ) : null}
           </div>
-          : null
-          }
         </div>
       </Fragment>
     );
